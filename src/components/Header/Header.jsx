51d9994c0c84af43c3ad57474dcb0d07747e381b
jcochran206 <jonathan.o.cochran@gmail.com>
2022-02-12 20:31:10 -0800

    modified header, added mui styles, hook to call styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles();
     return(
         <div>
-            <h1>Header</h1>
+            <AppBar position ="static">
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h5" className={classes.title}>
+                        Travel Companion
+                    </Typography>
+                </Toolbar>
+                <Box display="flex">
+                    <Toolbar className={classes.toolbar}>
+                        <Typography variant="h6" className={classes.title}>
+                            Explore new places
+                        </Typography>
+                    </Toolbar> 
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput }} />
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </AppBar>
         </div>
     )
 }
